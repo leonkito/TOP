a106d6e1d475ade82fd8da0290d87128979bd93d
Leonardo Niekiforuk <leonardoniekiforuk@hotmail.com>
2022-09-09 14:09:02 -0300

    adding-removing and changing status working

--- Library/script.js
+++ Library/script.js
@@ -2,9 +2,7 @@ let Library = [];
 let Stand = [];
 const stand = document.getElementById("stand");
 const submitb = document.getElementById('submitBtn')
-submitb.addEventListener("click", () => {
-	addBook();
-});
+submitb.addEventListener("click", () => {addBook();});
 
 const inputGet = () =>{
   const bookTitle   = document.getElementById("bookTitle").value;
@@ -18,9 +16,10 @@ function addBook () {
   Library.push(newBook);
   updatebooks();
 }
-const Book = (title,author,pages)=>{
+const Book = (title,author,pages,status)=>{
+  status = false
   //const tellPages = () => console.log(`${title} has ${pages} pages`);
-  return {title,author,pages};
+  return {title,author,pages,status};
 }
 
 const createCards = (book,indexT) =>{
@@ -28,21 +27,34 @@ const createCards = (book,indexT) =>{
   const title = document.createElement('p');
   const author = document.createElement('p');
   const pages = document.createElement('p');
+  const read = document.createElement('button');
+  const btn = document.createElement('button');
   title.classList.add('title');
   title.textContent = `Title: ${book.title}`
   author.classList.add('author');
   author.textContent = `Author: ${book.author}`
   pages.classList.add('pages');
   pages.textContent = `N° of pages: ${book.pages}`
+  btn.textContent = 'remove'
+  read.textContent = 'not read'
   card.appendChild(author)
-  const btn = document.createElement('button');
   card.appendChild(title)
   card.appendChild(pages)
+  read.onclick = function(){toggleRead('t'+indexT)};
+  read.setAttribute('id',('t'+indexT));
   btn.onclick = function(){myScript(indexT)};
-  btn.textContent = `Indice ${indexT}`
+  card.appendChild(read)
   card.appendChild(btn)
   card.setAttribute('id',indexT)
   stand.appendChild(card).className = 'card';
+}
+
+function toggleRead(indice){
+  if (document.getElementById(indice).textContent === 'read') {
+    document.getElementById(indice).textContent = 'not read'
+  } else {
+    document.getElementById(indice).textContent = 'read'
+  }
 }
 const updatebooks = () => {
   for (let book of Library) {
@@ -52,11 +64,12 @@ const updatebooks = () => {
     }
   }
 }
+
 function myScript(indice){
   document.getElementById(indice).remove();
+  //Stand.splice(indice,1);
 }
 
-//verify how to add book only once to the stand or to redifine stand every time a new book is add
 function containsObject(obj, list) {
   return list.some(elem => elem === obj)
 }
