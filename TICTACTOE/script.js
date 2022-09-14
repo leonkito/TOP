2a16790794d67d855f6556049ae62dc912fc8645
Leonardo Niekiforuk <leonardoniekiforuk@hotmail.com>
2022-09-14 17:05:25 -0300

    Added all winning conditions

--- TICTACTOE/script.js
+++ TICTACTOE/script.js
@@ -1,4 +1,5 @@
 const board = document.getElementById("main");
+
 createGame();
 function createGame (){
   for(let i = 0; i<=8;i++){
@@ -10,33 +11,45 @@ function createGame (){
   }
 }
 
-const Player = (type)=>{
-  return(type)
-}
-let player = 'x'
+let player
 let Board =[0,0,0,0,0,0,0,0,0]
 function makeMove(i){
   const tile = document.getElementById(i);
-  if (controlBoard(i)){
+  if (Board[i]===0){
+    player = player === 'X' ? 'O' : 'X'
     tile.textContent = player
     Board[i]=player
     checkwinner()
-    if (player ==='x'){
-      player='O'
-    } else{
-      player='x'
-    }
   }
 }
-function checkwinner(){
-  if (Board[0] === Board[1] && Board[0] === Board[2]){
-    console.log(Board[0] + ' wins')
-  }
 
-  
-}
-function controlBoard (index){
-  if (Board[index]===0){
-    return(true)
+function checkwinner(){
+  let roundWon = false;
+  for (let i = 0; i <= 7; i++){
+    const winCondition = winningConditions[i];
+    let a = Board[winCondition[0]];
+    let b = Board[winCondition[1]];
+    let c = Board[winCondition[2]];
+    if (a === 0 || b === 0 || c===0){
+      continue
+    }        
+    if (a === b && b === c) {
+      roundWon = true;
+      break
+    } 
+  }
+  if (roundWon){
+    document.getElementById("scoreboard").innerText = player + 'WIN'
   }
 }
+
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
